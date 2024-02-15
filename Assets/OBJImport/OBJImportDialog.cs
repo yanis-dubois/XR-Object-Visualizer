9026654c7c906c2f610d6aaeb620a7e79f54656b
Dubois Yanis <yanis.dubois@etu.u-bordeaux.fr>
2024-02-15 11:47:29 +0100

    imported objects are now interactable

--- Assets/OBJImport/OBJImportDialog.cs
+++ Assets/OBJImport/OBJImportDialog.cs
@@ -8,8 +8,8 @@ using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Transformers;
 
 public class OBJImportDialog : MonoBehaviour {
+    public GameObject interactableObjectPrefab;
     public GameObject objectSpawner; 
-    GameObject loadedObject;
 
     private void Start() {
         FileBrowser.SetFilters(true, new FileBrowser.Filter("Text Files", ".obj"));
@@ -26,8 +26,6 @@ public class OBJImportDialog : MonoBehaviour {
                 if (!File.Exists(filePath)) {
                     log = "File doesn't exist.";
                 } else {
-                    if (loadedObject != null)            
-                        Destroy(loadedObject);
                     OpenObjFromPath(filePath);
                     log = "File loaded";
                 }
@@ -42,34 +40,59 @@ public class OBJImportDialog : MonoBehaviour {
     }
 
     private void OpenObjFromPath(string filePath) {
-        var loadedObj = new OBJLoader().Load(filePath);
+        var tmpObj = new OBJLoader().Load(filePath);
 
         // move object in the scene tree
-        loadedObj.transform.parent = objectSpawner.transform;
-        
-        foreach (Transform child in loadedObj.transform) {
+        tmpObj.transform.parent = objectSpawner.transform;
+
+        foreach (Transform child in tmpObj.transform) {
+            // instantiate prefab and change mesh
+            GameObject obj = Instantiate(interactableObjectPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            obj.GetComponent<MeshFilter>().mesh = child.GetComponent<MeshFilter>().mesh;
+            obj.transform.parent = objectSpawner.transform;
+            
             // rescale and move object
-            Vector3 size = child.GetComponent<Renderer>().bounds.size;
+            Vector3 size = obj.GetComponent<Renderer>().bounds.size;
             float maxDim = Mathf.Max(size.x, Mathf.Max(size.y, size.z));
-            child.transform.localScale = new Vector3(1.0f/maxDim, 1.0f/maxDim, 1.0f/maxDim);
-            Vector3 position = child.GetComponent<Renderer>().bounds.center;
-            child.transform.position += Vector3.one - position;
-
-            // add physics
-            BoxCollider boxCollider = child.gameObject.AddComponent<BoxCollider>();
-            Rigidbody rigidbody = child.gameObject.AddComponent<Rigidbody>();
-            rigidbody.isKinematic = true;
-            rigidbody.useGravity = false;
-
-            // make it interactable
-            XRGrabInteractable grabInteractable = child.gameObject.AddComponent<XRGrabInteractable>();
-            XRGeneralGrabTransformer grabTransformer = child.gameObject.AddComponent<XRGeneralGrabTransformer>();
-            // ...
+            obj.transform.localScale = new Vector3(0.2f / maxDim, 0.2f / maxDim, 0.2f / maxDim);
+            Vector3 position = obj.GetComponent<Renderer>().bounds.center;
+            obj.transform.position += Vector3.one - position;
 
             // add material
             var mat = new Material(Shader.Find("Universal Render Pipeline/Lit"));
             child.GetComponent<Renderer>().material = mat; 
-            // ...
         }
+
+        // suppr tmp object
+        Destroy(tmpObj);
+        
+        // foreach (Transform child in loadedObj.transform) {
+        //     
+
+        //     // add physics
+        //     BoxCollider boxCollider = child.gameObject.AddComponent<BoxCollider>();
+        //     Rigidbody rigidbody = child.gameObject.AddComponent<Rigidbody>();
+        //     rigidbody.isKinematic = true;
+        //     rigidbody.useGravity = false;
+
+        //     // make it interactable
+        //     XRGrabInteractable grabInteractable = child.gameObject.AddComponent<XRGrabInteractable>();
+        //     // select mod - multiple
+        //     grabInteractable.selectMode = InteractableSelectMode.Multiple;
+        //     // focus mod - multiple
+        //     // ???
+        //     // use dynamic attach - true
+        //     grabInteractable.useDynamicAttach = true;
+        //     // add default grab transformer - false
+        //     grabInteractable.addDefaultGrabTransformers = false;
+        //     // starting multiple grab transformer - grabTransformer
+        //     XRGeneralGrabTransformer grabTransformer = child.gameObject.AddComponent<XRGeneralGrabTransformer>();
+        //     grabInteractable.startingMultipleGrabTransformers = grabTransformer;
+
+        //     // add material
+        //     var mat = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+        //     child.GetComponent<Renderer>().material = mat; 
+        //     // ...
+        // }
     }
 }
--- Assets/OBJImport/Samples/ObjFromStream.cs
+++ Assets/OBJImport/Samples/ObjFromStream.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class ObjFromStream : MonoBehaviour {
 
+    public GameObject interactableObjectPrefab;
     public GameObject objectSpawner; 
 
 	void Start () {
@@ -20,19 +21,40 @@ public class ObjFromStream : MonoBehaviour {
         // create stream and load object
         var stream_obj = new MemoryStream(Encoding.UTF8.GetBytes(www_obj.text));
         var stream_mtl = new MemoryStream(Encoding.UTF8.GetBytes(www_mtl.text));
-        var loadedObj = new OBJLoader().Load(stream_obj, stream_mtl);
+        var tmpObj = new OBJLoader().Load(stream_obj, stream_mtl);
 
-        // make object interactable
-        loadedObj.transform.parent = objectSpawner.transform;
-        
-        foreach (Transform child in loadedObj.transform) {
-            // rescale
-            Vector3 size = child.GetComponent<Renderer>().bounds.size;
-            child.transform.localScale = new Vector3(1.0f/size.x, 1.0f/size.y, 1.0f/size.z);
+        foreach (Transform child in tmpObj.transform) {
+            // instantiate prefab and change mesh
+            GameObject obj = Instantiate(interactableObjectPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            obj.GetComponent<MeshFilter>().mesh = child.GetComponent<MeshFilter>().mesh;
+            obj.transform.parent = objectSpawner.transform;
+            
+            // rescale and move object
+            Vector3 size = obj.GetComponent<Renderer>().bounds.size;
+            float maxDim = Mathf.Max(size.x, Mathf.Max(size.y, size.z));
+            obj.transform.localScale = new Vector3(0.2f / maxDim, 0.2f / maxDim, 0.2f / maxDim);
+            Vector3 position = obj.GetComponent<Renderer>().bounds.center;
+            obj.transform.position += Vector3.one - position;
 
-            // move 
-            Vector3 position = child.GetComponent<Renderer>().bounds.center;
-            child.transform.position += Vector3.one - position;
+            // add material
+            var mat = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+            child.GetComponent<Renderer>().material = mat; 
         }
+
+        // suppr tmp object
+        Destroy(tmpObj);
+
+        // // make object interactable
+        // loadedObj.transform.parent = objectSpawner.transform;
+        
+        // foreach (Transform child in loadedObj.transform) {
+        //     // rescale
+        //     Vector3 size = child.GetComponent<Renderer>().bounds.size;
+        //     child.transform.localScale = new Vector3(1.0f/size.x, 1.0f/size.y, 1.0f/size.z);
+
+        //     // move 
+        //     Vector3 position = child.GetComponent<Renderer>().bounds.center;
+        //     child.transform.position += Vector3.one - position;
+        // }
 	}
 }
