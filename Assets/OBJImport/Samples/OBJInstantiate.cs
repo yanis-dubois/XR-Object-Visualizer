f3cc6ae6e17a94d53524f46bfc41264d0bc42bed
Dubois Yanis <yanis.dubois@etu.u-bordeaux.fr>
2024-02-16 14:37:15 +0100

    collider and shader fix

---
+++ Assets/OBJImport/Samples/OBJInstantiate.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using Dummiesman;
+
+public class OBJInstantiate : MonoBehaviour {
+
+    public static void instantiate(GameObject interactableObjectPrefab, 
+            GameObject objectSpawner, GameObject tmpObj) {
+        
+        // move object in the scene tree
+        tmpObj.transform.parent = objectSpawner.transform;
+
+        foreach (Transform child in tmpObj.transform) {
+            // instantiate prefab and change mesh
+            GameObject obj = Instantiate(interactableObjectPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            obj.GetComponent<MeshFilter>().mesh = child.GetComponent<MeshFilter>().mesh;
+            obj.transform.parent = objectSpawner.transform;
+            
+            // rescale and move object
+            Vector3 size = obj.GetComponent<Renderer>().bounds.size;
+            Vector3 position = obj.GetComponent<Renderer>().bounds.center;
+
+            obj.GetComponent<BoxCollider>().size = size;
+            obj.GetComponent<BoxCollider>().center = position;
+
+            float maxDim = Mathf.Max(size.x, Mathf.Max(size.y, size.z));
+            obj.transform.localScale = new Vector3(1.0f / maxDim, 1.0f / maxDim, 1.0f / maxDim);
+            obj.transform.position = new Vector3(0.5f, 1, 0.5f);
+        }
+
+        Destroy(tmpObj);
+    }
+}
--- Assets/OBJImport/Samples/ObjFromStream.cs
+++ Assets/OBJImport/Samples/ObjFromStream.cs
@@ -16,41 +16,9 @@ public class ObjFromStream : MonoBehaviour {
         
         // create stream and load object
         var stream_obj = new MemoryStream(Encoding.UTF8.GetBytes(www_obj.text));
+
+        // instantiate it
         var tmpObj = new OBJLoader().Load(stream_obj);
-
-        foreach (Transform child in tmpObj.transform) {
-            // instantiate prefab and change mesh
-            GameObject obj = Instantiate(interactableObjectPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-            obj.GetComponent<MeshFilter>().mesh = child.GetComponent<MeshFilter>().mesh;
-            obj.transform.parent = objectSpawner.transform;
-            
-            // rescale and move object
-            Vector3 size = obj.GetComponent<Renderer>().bounds.size;
-            obj.GetComponent<BoxCollider>().size = size;
-            float maxDim = Mathf.Max(size.x, Mathf.Max(size.y, size.z));
-            obj.transform.localScale = new Vector3(1.0f / maxDim, 1.0f / maxDim, 1.0f / maxDim);
-            Vector3 position = obj.GetComponent<Renderer>().bounds.center;
-            obj.transform.position += new Vector3(0.5f, 1, 0.5f);
-
-            // add material
-            var mat = new Material(Shader.Find("Universal Render Pipeline/Lit"));
-            child.GetComponent<Renderer>().material = mat; 
-        }
-
-        // suppr tmp object
-        Destroy(tmpObj);
-
-        // // make object interactable
-        // loadedObj.transform.parent = objectSpawner.transform;
-        
-        // foreach (Transform child in loadedObj.transform) {
-        //     // rescale
-        //     Vector3 size = child.GetComponent<Renderer>().bounds.size;
-        //     child.transform.localScale = new Vector3(1.0f/size.x, 1.0f/size.y, 1.0f/size.z);
-
-        //     // move 
-        //     Vector3 position = child.GetComponent<Renderer>().bounds.center;
-        //     child.transform.position += Vector3.one - position;
-        // }
+        OBJInstantiate.instantiate(interactableObjectPrefab, objectSpawner, tmpObj);
 	}
 }
