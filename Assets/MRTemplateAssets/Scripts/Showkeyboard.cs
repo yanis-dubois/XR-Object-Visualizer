ff2fffc3d9aa760d88ffa274f903007c3d6e2a3e
Bernadet <florian.bernadet@etu.u-bordeaux.fr>
2024-03-07 15:38:52 +0100

    add keyboard and inputField

---
+++ Assets/MRTemplateAssets/Scripts/Showkeyboard.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Microsoft.MixedReality.Toolkit.Experimental.UI;
+
+public class KeyboardController : MonoBehaviour
+{
+    private TMP_InputField inputField;
+
+    void Start(){
+        inputField=GetComponent<TMP_InputField>();
+        inputField.onSelect.AddListener(x => OpenKeyboard());
+    }
+
+    public void OpenKeyboard(){
+        NonNativeKeyboard.Instance.InputField=inputField;
+        NonNativeKeyboard.Instance.PresentKeyboard(inputField.text);
+    }
+}
+
